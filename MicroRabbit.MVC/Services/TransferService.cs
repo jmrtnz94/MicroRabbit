6077efef5c583a33a96bb9bf7804b909d50723ec
jose.martinez <jose.martinez@spreetail.com>
2023-08-04 09:24:34 -0500

    Add MicroRabbit.MVC

---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MicroRabbit.MVC.Models.DTO;
+
+namespace MicroRabbit.MVC.Services;
+
+public class TransferService : ITransferService
+{
+    private readonly HttpClient _apiClient;
+
+    public TransferService(HttpClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    public async Task Transfer(TransferDto transferDto)
+    {
+        const string uri = "https://localhost:7216/api/Banking";
+        var transferContent = new StringContent(JsonSerializer.Serialize(transferDto),
+            Encoding.UTF8, "application/json");
+        
+        var response = await _apiClient.PostAsync($"{uri}", transferContent);
+        response.EnsureSuccessStatusCode();
+    }
+}
